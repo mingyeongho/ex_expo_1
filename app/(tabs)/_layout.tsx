a9e5b7d13d74255368fcb89ede7da51c353f2fc6
민경호 <1736s@naver.com>
2024-11-18 22:23:21 +0900

    feat: 탭 네비게이션 구현

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,76 @@
-import { View, Text } from "react-native";
+import { Text, Image } from "react-native";
 import React from "react";
+import { Tabs } from "expo-router";
+import { icons } from "@/constants";
+import { StatusBar } from "expo-status-bar";
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 84,
+          },
+        }}
+      >
+        {[
+          {
+            name: "home",
+            icon: icons.home,
+            label: "Home",
+          },
+          {
+            name: "bookmark",
+            icon: icons.bookmark,
+            label: "Bookmark",
+          },
+          {
+            name: "create",
+            icon: icons.plus,
+            label: "Create",
+          },
+          {
+            name: "profile",
+            icon: icons.profile,
+            label: "Profile",
+          },
+        ].map((tab) => (
+          <Tabs.Screen
+            key={tab.name}
+            name={tab.name}
+            options={{
+              title: tab.label,
+              headerShown: false,
+              tabBarIcon: ({ color }) => (
+                <Image
+                  source={tab.icon}
+                  resizeMode="contain"
+                  tintColor={color}
+                  className="w-6 h-6"
+                />
+              ),
+              tabBarLabel: ({ color, focused }) => (
+                <Text
+                  className={`${
+                    focused ? "font-psemibold" : "font-pregular"
+                  } text-xs mt-0.5`}
+                  style={{ color }}
+                >
+                  {tab.label}
+                </Text>
+              ),
+            }}
+          />
+        ))}
+      </Tabs>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
   );
 };
 
