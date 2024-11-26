57eea834ff2cd3af90758097750b41e486eeda62
민경호 <1736s@naver.com>
2024-11-26 11:20:11 +0900

    feat: StatusBar 제거
    - 따로 설정을 안해줘도 상단 상태바가 제대로 보임

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -9,7 +9,6 @@ const AuthLayout = () => {
         <Stack.Screen name="sign-in" options={{ headerShown: false }} />
         <Stack.Screen name="sign-up" options={{ headerShown: false }} />
       </Stack>
-      <StatusBar backgroundColor="#161622" style="light" />
     </>
   );
 };
