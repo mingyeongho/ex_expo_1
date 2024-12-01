e65cd3c85325bb23e2af66cc87fd0fb5f983804b
민경호 <1736s@naver.com>
2024-12-01 17:09:42 +0900

    feat: 프로필 페이지

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,21 +1,49 @@
 import { icons } from "@/constants";
-import { Button } from "@/src/components";
+import { EmptyState, InfoBox, VideoCard, VideoType } from "@/src/components";
 import { supabase } from "@/src/lib/supabase";
 import { User } from "@supabase/supabase-js";
 import { router } from "expo-router";
 import { useEffect, useState } from "react";
-import { View } from "react-native";
-import { Alert, Image, Text } from "react-native";
+import { FlatList, TouchableOpacity, View } from "react-native";
+import { Alert, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
   const [user, setUser] = useState<User | null>(null);
+  const [videos, setVideos] = useState<VideoType[]>([]);
 
   const signOut = async () => {
     const { error } = await supabase.auth.signOut();
 
     if (error) Alert.alert(error.message);
     router.push("/(auth)/sign-in");
+  };
+
+  const onFetchMyVideos = async () => {
+    if (!user?.id) return;
+
+    const { data: fetchedVideos, error } = await supabase
+      .from("videos")
+      .select(
+        `
+      *,
+      users (username)
+    `
+      )
+      .eq("creator", user?.id);
+
+    if (error) throw error;
+
+    const formatVideos = fetchedVideos.map((video) => ({
+      id: video.id,
+      title: video.title,
+      thumbnail: video.thumbnail,
+      prompt: video.prompt,
+      video: video.video,
+      username: video.users?.username || "",
+    }));
+
+    setVideos(formatVideos || []);
   };
 
   useEffect(() => {
@@ -30,25 +58,60 @@ const Profile = () => {
 
       signOut();
     })();
+    onFetchMyVideos();
   }, []);
 
   return (
-    <SafeAreaView className="items-center justify-center bg-white flex-1">
-      <View className="text-3xl font-pblack">
-        {user?.id && (
-          <>
-            <Image
-              source={icons.profile}
-              resizeMode="contain"
-              className="w-6 h-6"
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={videos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <VideoCard {...item} />}
+        ListHeaderComponent={
+          <View className="w-full flex justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity
+              onPress={signOut}
+              className="flex w-full items-end mb-10"
+            >
+              <Image
+                source={icons.logout}
+                resizeMode="contain"
+                className="w-6 h-6"
+              />
+            </TouchableOpacity>
+
+            <View className="w-16 h-16 border border-secondary rounded-lg flex justify-center items-center">
+              <Image
+                source={icons.profile}
+                className="w-[90%] h-[90%] rounded-lg"
+                resizeMode="cover"
+              />
+            </View>
+
+            <InfoBox
+              title={user?.user_metadata.username || ""}
+              containerStyles="mt-5"
+              titleStyles="text-lg"
             />
-            <Text className="font-psemibold text-xl">
-              {user.user_metadata.username}
-            </Text>
-          </>
+
+            <View className="mt-5 flex flex-row">
+              <InfoBox
+                title={videos.length.toString() || "0"}
+                subtitle="Posts"
+                titleStyles="text-xl"
+                containerStyles="mr-10"
+              />
+              <InfoBox title="0" subtitle="Followers" titleStyles="text-xl" />
+            </View>
+          </View>
+        }
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subTitle="Be the first one to upload a video"
+          />
         )}
-      </View>
-      <Button label="SignOut" onClick={signOut}></Button>
+      />
     </SafeAreaView>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./Button";
 export * from "./EmptyState";
 export * from "./FormField";
+export * from "./InfoBox";
 export * from "./SearchInput";
 export * from "./Trending";
 export * from "./VideoCard";
